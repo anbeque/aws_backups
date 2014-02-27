ad13c069f9661c41bb7d7680cda4cccb37e41f7d
James Gartrell <jgartrel@financialforce.com>
2014-02-26 16:09:39 -0800

    Start using ec2 as source data, but still do not make changes

--- test.rb
+++ test.rb
@@ -195,16 +195,19 @@ end
 @ec2   = RightAws::Ec2.new(@aws_key,@aws_secret)
 
 vols = []
-#vols += @ec2.describe_volumes(:filters => { 'tag-key' => 'backup:enabled' })
+vols += @ec2.describe_volumes(:filters => { 'tag-key' => 'backup:enabled' })
 #vols += @ec2.describe_volumes(:filters => { 'volume-id' => 'vol-63d1f029' })
 
-vols = YAML.load_file("fixtures/vols.yaml")
+#vols = YAML.load_file("fixtures/vols.yaml")
 vols.each do |v|
   obj = Backup.from_ec2(v)
-  #obj.api = @ec2
-  obj.parse_ec2_snaps(YAML.load_file("fixtures/snaps.yaml")) 
+  obj.api = @ec2
+  obj.parse_ec2_snaps(@ec2.describe_snapshots(:filters => { 'volume-id' => obj.id }))
+  #obj.parse_ec2_snaps(YAML.load_file("fixtures/snaps.yaml"))
+  obj.api = nil
   obj.backup
   obj.prune_snaps!
+  puts "Current Latest:"
   pp obj.latest
 end
 
