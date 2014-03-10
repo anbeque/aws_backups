5eabb3520d95fbccc41bef8a3afaf119db60f7f8
James Gartrell <jgartrel@financialforce.com>
2014-03-10 11:58:56 -0700

    Remove unnecessary commented code

--- test.rb
+++ test.rb
@@ -14,14 +14,11 @@ require './lib/backup.rb'
 
 vols = []
 vols += @ec2.describe_volumes(:filters => { 'tag:backup:enabled' => '1' })
-#vols += @ec2.describe_volumes(:filters => { 'volume-id' => 'vol-63d1f029' })
 
-#vols = YAML.load_file("fixtures/vols.yaml")
 vols.each do |v|
   obj = Backup.from_ec2(v, :logger => @logger)
   obj.api = @ec2
   obj.parse_ec2_snaps(@ec2.describe_snapshots(:filters => { 'volume-id' => obj.id }))
-  #obj.parse_ec2_snaps(YAML.load_file("fixtures/snaps.yaml"))
   obj.api = nil
   obj.backup
   obj.prune_snaps!
