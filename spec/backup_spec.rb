602d61ea9d27e8287ec3d3a845aef82ffda25882
James Gartrell <jgartrel@financialforce.com>
2014-03-14 08:04:08 -0700

    Move fixtures and improve readability for let statements

--- spec/backup_spec.rb
+++ spec/backup_spec.rb
@@ -5,10 +5,19 @@ describe Backup do
     include RSpec::Mocks::ExampleMethods
     $logger = Logger.new(STDOUT)   if !$logger
     $logger.level = Logger::WARN
-    let(:vols)  { YAML.load_file("./fixtures/vols.yaml") }
-    let(:snaps) { YAML.load_file("./fixtures/snaps.yaml") }
-    let(:new_snap) { YAML.load_file("./fixtures/pending_snap.yaml") }
-    let(:ec2) { RightAws::Ec2.new("invalid_key","invalid_secret", :logger => $logger) }
+    let(:vols) {
+      YAML.load_file(File.join(File.dirname(__FILE__),'fixtures','vols.yaml'))
+     }
+    let(:snaps) {
+      YAML.load_file(File.join(File.dirname(__FILE__),'fixtures','snaps.yaml'))
+     }
+    let(:new_snap) {
+      YAML.load_file(File.join(
+        File.dirname(__FILE__),'fixtures','pending_snap.yaml'))
+     }
+    let(:ec2) {
+      RightAws::Ec2.new("invalid_key","invalid_secret", :logger => $logger)
+     }
     let(:backup) do
       Time.stub(:now).and_return(Time.parse("2014-02-03T10:15:00.000Z"))
       obj = Backup.from_ec2(vols.first, :logger => $logger)
